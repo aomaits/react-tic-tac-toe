d7ac7d2de5b1c86c59f1013baaa1332d3f934b35
Abe Omaits <aomaits@gmail.com>
2024-02-08 13:00:25 -0600

    small cleanup and notes as I reviewed code

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Square({ value, onSquareClick, isUsed, square}) {
   const squareStyle = {
-    color: isUsed ? (value === 'X' ? 'blue' : 'orange') : 'inherit'
+    color: isUsed ? (value === 'X' ? 'blue' : 'orange') : 'inherit' //inherit here meaning "inherit this styling set in the parent element"
   }
   return (
     <button className={square} style={squareStyle} onClick={onSquareClick}>
